9bc2795a85d922342cde3d741083be3a2df0b071
yudhawahyup27 <wyudha104@gmail.com>
2026-06-11 22:24:52 +0700

    fix: read Firebase credentials at runtime on Vercel

---
+++ .nuxt/dist/client/_nuxt/Dym_UFJs.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./Del23NVa.js";import{_ as c,M as e,A as t}from"./BKZacYIl.js";const _={};function r(s,a){const n=o;return t(),e(n)}const m=c(_,[["render",r]]);export{m as default};
